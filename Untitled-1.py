7a8db6298c8c9751d34c322df349dd5cf2de8fa4
AntiCaffe <dongjun6288@gmail.com>
2023-01-06 15:20:26 +0900

    main_name

--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import numpy as np
-image1 = Image.open("Ysigon.jpg")
-image2 = Image.open("cubic_Ysigon.jpg")
-pix1 = np.array(image1, dtype=np.uint8)
-pix2 = np.array(image2, dtype=np.uint8)
-print(pix1.size,pix2.size)
